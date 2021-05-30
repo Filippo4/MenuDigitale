ffae2e57ab4e56022a1f13e7fd1eb77664630a70
Filippo <Dell@DESKTOP-2751OC0>
2021-05-30 22:30:01 +0200

    Aggiungere i file di progetto.

---
+++ MenuDigitale/ConfermaComanda.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MenuDigitale
+{
+    /// <summary>
+    /// Logica di interazione per ConfermaComanda.xaml
+    /// </summary>
+    public partial class ConfermaComanda : Window
+    {
+        List<string> comanda = new List<string>();
+        public ConfermaComanda(List<string> comanda)
+        {
+            InitializeComponent();
+            for (int i = 0; i < comanda.Count; i++)
+                ltb_comanda.Items.Add(comanda[i].ToString());
+        }
+
+        private void btn_inviacomanda_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("la comanda è stata inviata!", "Informazione", MessageBoxButton.OK, MessageBoxImage.Information);
+            this.Close();
+        }
+
+        private void btn_esci_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_cancellaComanda_Click(object sender, RoutedEventArgs e)
+        {
+            ltb_comanda.Items.Clear();
+
+            comanda.Clear();
+            this.Close();
+        }
+    }
+}
---
+++ MenuDigitale/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuDigitale
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        List<string> comanda = new List<string>();
+
+        private void add_primi_1_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Tagliatelle al ragu");
+        }
+
+        private void add_primi_2_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Penne all'arrabbiata");
+        }
+
+        private void add_primi_3_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Gnocchi al pomodoro");
+        }
+
+        private void add_primi_4_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Tortellini alla norcina");
+        }
+
+        private void add_primi_5_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Spaghetti alla carbonara");
+        }
+
+        private void add_secondi_1_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Tagliata");
+        }
+
+        private void add_secondi_2_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Filetto al pepe rosa");
+        }
+
+        private void add_secondi_3_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Bistecca alla fiorentina");
+        }
+
+        private void add_secondi_4_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Grigliata mista");
+
+        }
+
+        private void add_secondi_5_Click(object sender, RoutedEventArgs e)
+        {
+            comanda.Add("Tartare di vitello");
+        }
+
+        private void Comanda_Click(object sender, RoutedEventArgs e)
+        {
+            new ConfermaComanda(comanda).ShowDialog();
+        }
+    }
+}
+}
